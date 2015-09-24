a071dbd0b1c04d8ba701ea87f190f07e3e2fc8f2
unknown <Nima.Moghadam@SYDLT5CG4520KVJ.corp.sita.aero>
2015-09-24 16:36:24 +1000

    no message

---
+++ bpim-bpmn2/bpim-bpmn2-engine/src/main/java/org/bpim/engine/TransformaionManager.java
@@ -0,0 +1,15 @@
+package org.bpim.engine;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TransformaionManager {
+	
+	protected static final Logger logger = LoggerFactory.getLogger(TransformaionManager.class);
+	
+	public static void transform(org.jbpm.workflow.instance.WorkflowProcessInstance processInstance, org.jbpm.workflow.instance.NodeInstance nodeInstance){
+		
+	}
+
+}
